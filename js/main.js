d69b17670fb4a5de21bbbfb1f46de17609b2648f
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-06 14:54:09 +0100

    prova funzione

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+
+
+const app = new Vue ({
+
+    el: '.app-container',
+    data: {
+        title: 'Hello there, my names is Pietro. Nice to meet you',
+        imageUrl: './img/linkedin.jpg',
+        helloCLass: 'hidden'
+    },
+
+    methods: {
+        pikaboo: function() {
+            this.helloCLass = (this.helloClass === 'hidden') ? 'show' : 'hidden';
+        }
+    }
+});
